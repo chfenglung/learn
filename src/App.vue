5d25e83b61a7e9394a39e356cb51662f0801dbdb
chung <chfenglung@gmail.com>
2025-05-19 16:06:41 +0800

    [功能] footer切版

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
   import { RouterView } from 'vue-router'
   import NavBar from './components/NavBar.vue'
+  import FooterView from './components/FooterView.vue'
 </script>
 
 <template>
   <NavBar></NavBar>
   <router-view id="view"></router-view>
+  <FooterView></FooterView>
 </template>
 
 <style lang="scss"></style>
---
+++ src/components/FooterView.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <section class="footer">© 2025 Alyse Wang. All rights reserved.</section>
+</template>
+
+<style lang="scss">
+  .footer {
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 150%;
+    text-align: center;
+    padding: 24px 0;
+  }
+</style>
