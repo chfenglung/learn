5bcf428d381b970c861168930e1b132363f90b04
chung <chfenglung@gmail.com>
2025-05-21 20:36:22 +0800

    [功能] 調整gitPages路徑

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,12 @@ import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/learn/', // 子目錄路徑
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets' // 確認資源目錄配置
-  },
+  base: '/blog/',
+  // base: '/learn/', // 子目錄路徑
+  // build: {
+  //   outDir: 'dist',
+  //   assetsDir: 'assets' // 確認資源目錄配置
+  // },
   plugins: [
     vue(),
     eslintPlugin({
