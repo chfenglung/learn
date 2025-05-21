e65a8bc8288e845cf077cb18e44c174b4b963365
chung <chfenglung@gmail.com>
2025-05-21 20:49:05 +0800

    [功能] router修正

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import BlogView from '../views/BlogView.vue'
 import DetailedView from '../views/DetailedView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
+  base: '/blog/',
   routes: [
     {
       path: '/',
